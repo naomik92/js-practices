4a60f38e1b7daf2c5b6cf684a931c3b123b88b70
naomik92 <161175746+naomik92@users.noreply.github.com>
2025-12-04 11:02:44 +0900

    1月を指定時に1月のカレンダーを表示させる

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -7,36 +7,39 @@ import { TZDate } from "@date-fns/tz";
 const args = minimist(process.argv.slice(2));
 
 const today = TZDate.tz("Asia/Tokyo");
-const cal_year = args.y || today.getFullYear();
-const cal_month = args.m - 1 || today.getMonth();
-
-const cal_date = new TZDate(cal_year, cal_month, "Asia/Tokyo");
-
-const first_date = startOfMonth(cal_date).getDate();
-const last_date = endOfMonth(cal_date).getDate();
-const first_day = startOfMonth(cal_date).getDay();
-
-const blank_array = [];
-const dates_array = [];
-const calendar_rows = [];
-const calendar_col_number = 7;
-
-for (let x = 0; x < first_day; x++) {
-  blank_array.push("   ");
+const calYear = args.y || today.getFullYear();
+let calMonth = today.getMonth();
+if (args.m) {
+  calMonth = args.m - 1;
 }
 
-for (let y = first_date; y <= last_date; y++) {
-  dates_array.push(y.toString().padStart(3, " "));
+const calDate = new TZDate(calYear, calMonth, "Asia/Tokyo");
+
+const firstDate = startOfMonth(calDate).getDate();
+const lastDate = endOfMonth(calDate).getDate();
+const firstDay = startOfMonth(calDate).getDay();
+
+const blankArray = [];
+const datesArray = [];
+const calendarRows = [];
+const calendarColNumber = 7;
+
+for (let x = 0; x < firstDay; x++) {
+  blankArray.push("   ");
 }
 
-const calendar_dates = blank_array.concat(dates_array);
-
-for (let z = 0; z < calendar_dates.length; z += calendar_col_number) {
-  calendar_rows.push(calendar_dates.slice(z, z + calendar_col_number));
+for (let y = firstDate; y <= lastDate; y++) {
+  datesArray.push(y.toString().padStart(3, " "));
 }
 
-console.log(`       ${cal_month + 1}月 ${cal_year}   `);
+const calendarDates = blankArray.concat(datesArray);
+
+for (let z = 0; z < calendarDates.length; z += calendarColNumber) {
+  calendarRows.push(calendarDates.slice(z, z + calendarColNumber));
+}
+
+console.log(`       ${calMonth + 1}月 ${calYear}   `);
 console.log(" 日 月 火 水 木 金 土");
-for (const calendar_row of calendar_rows) {
-  console.log(calendar_row.join(""));
+for (const calendarRow of calendarRows) {
+  console.log(calendarRow.join(""));
 }
