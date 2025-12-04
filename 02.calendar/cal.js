0504c0549229d0fbea60038ef23e27150447d2bd
naomik92 <161175746+naomik92@users.noreply.github.com>
2025-12-04 12:02:19 +0900

    ライブラリをnamespace importに変更

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -1,34 +1,33 @@
 #!/usr/bin/env node
 
 import minimist from "minimist";
-import { startOfMonth, endOfMonth } from "date-fns";
-import { TZDate } from "@date-fns/tz";
+import * as dateFns from "date-fns";
+import * as dateFnsTz from "@date-fns/tz";
 
 const args = minimist(process.argv.slice(2));
 
-const today = TZDate.tz("Asia/Tokyo");
 const calYear = args.y || today.getFullYear();
 let calMonth = today.getMonth();
 if (args.m) {
   calMonth = args.m - 1;
 }
+const today = dateFnsTz.TZDate.tz("Asia/Tokyo");
 
-const calDate = new TZDate(calYear, calMonth, "Asia/Tokyo");
+const calDate = new dateFnsTz.TZDate(calYear, calMonth - 1, "Asia/Tokyo");
 
-const firstDate = startOfMonth(calDate).getDate();
-const lastDate = endOfMonth(calDate).getDate();
-const firstDay = startOfMonth(calDate).getDay();
+const firstDate = dateFns.startOfMonth(calDate);
+const lastDate = dateFns.endOfMonth(calDate);
 
 const blankArray = [];
 const datesArray = [];
 const calendarRows = [];
 const calendarColNumber = 7;
 
-for (let x = 0; x < firstDay; x++) {
+for (let x = 0; x < firstDate.getDay(); x++) {
   blankArray.push("   ");
 }
 
-for (let y = firstDate; y <= lastDate; y++) {
+for (let y = firstDate.getDate(); y <= lastDate.getDate(); y++) {
   datesArray.push(y.toString().padStart(3, " "));
 }
 
@@ -38,7 +37,7 @@ for (let z = 0; z < calendarDates.length; z += calendarColNumber) {
   calendarRows.push(calendarDates.slice(z, z + calendarColNumber));
 }
 
-console.log(`       ${calMonth + 1}月 ${calYear}   `);
+console.log(`       ${calMonth}月 ${calYear}   `);
 console.log(" 日 月 火 水 木 金 土");
 for (const calendarRow of calendarRows) {
   console.log(calendarRow.join(""));
