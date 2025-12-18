ea156bf95a54c9c33fb0aead18ad08ae0d9d2f74
naomik92 <161175746+naomik92@users.noreply.github.com>
2025-12-18 10:59:33 +0900

    レコードを追加し、自動採番されたIDを出力する記述を修正する

--- 03.asynchronous/async.js
+++ 03.asynchronous/async.js
@@ -5,11 +5,10 @@ const sqlite3 = sqlite3Module.verbose();
 const db = new sqlite3.Database(":memory:");
 
 db.run(
-  "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL)",
+  "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
   function () {
-    db.run("INSERT INTO books(title) VALUES('n-book')");
-    db.get("SELECT * FROM books", function (err, row) {
-      console.log(row.id);
+    db.run("INSERT INTO books(title) VALUES('n-book')", function () {
+      console.log(this.lastID);
       db.all("SELECT * FROM books", function (err, row) {
         console.log(row);
         db.run("DROP TABLE books");
