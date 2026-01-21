35b728c545715ff500119607515da49bc9c95df9
naomik92 <161175746+naomik92@users.noreply.github.com>
2026-01-21 09:45:49 +0900

    async_await_program.jsでトップレベルawaitを使うよう修正

--- 03.asynchronous/async_await_program.js
+++ 03.asynchronous/async_await_program.js
@@ -3,46 +3,42 @@
 import sqlite3 from "sqlite3";
 import { dbRun, dbAll, dbClose } from "./promise-based_sqlite3_functions.js";
 
-async function main() {
-  const db = new sqlite3.Database(":memory:");
-  await dbRun(
-    db,
-    "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-  );
-  const this_object = await dbRun(
-    db,
-    "INSERT INTO books(title) VALUES('n-book')",
-  );
-  console.log(this_object.lastID);
-  const rows = await dbAll(db, "SELECT * FROM books");
-  console.log(rows);
-  await dbRun(db, "DROP TABLE books");
+const db = new sqlite3.Database(":memory:");
+await dbRun(
+  db,
+  "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+);
+const this_object = await dbRun(
+  db,
+  "INSERT INTO books(title) VALUES('n-book')",
+);
+console.log(this_object.lastID);
+const rows = await dbAll(db, "SELECT * FROM books");
+console.log(rows);
+await dbRun(db, "DROP TABLE books");
 
-  await dbRun(
-    db,
-    "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-  );
+await dbRun(
+  db,
+  "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+);
+await dbRun(db, "INSERT INTO books(title) VALUES('n-book')");
+try {
   await dbRun(db, "INSERT INTO books(title) VALUES('n-book')");
-  try {
-    await dbRun(db, "INSERT INTO books(title) VALUES('n-book')");
-  } catch (err) {
-    if (err instanceof Error && err.code.startsWith("SQLITE_")) {
-      console.error(err.message);
-    } else {
-      throw err;
-    }
+} catch (err) {
+  if (err instanceof Error && err.code.startsWith("SQLITE_")) {
+    console.error(err.message);
+  } else {
+    throw err;
   }
-  try {
-    await dbAll(db, "SELECT * FROM book");
-  } catch (err) {
-    if (err instanceof Error && err.code.startsWith("SQLITE_")) {
-      console.error(err.message);
-    } else {
-      throw err;
-    }
-  }
-  await dbRun(db, "DROP TABLE books");
-  await dbClose(db);
 }
-
-main();
+try {
+  await dbAll(db, "SELECT * FROM book");
+} catch (err) {
+  if (err instanceof Error && err.code.startsWith("SQLITE_")) {
+    console.error(err.message);
+  } else {
+    throw err;
+  }
+}
+await dbRun(db, "DROP TABLE books");
+await dbClose(db);
