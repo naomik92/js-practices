5917535a480594769e4198edfd0d443764a2b283
naomik92 <161175746+naomik92@users.noreply.github.com>
2026-07-04 16:04:37 +0900

    display.jsの共通部分を別のメソッドに切り出す

--- 04.class/display.js
+++ 04.class/display.js
@@ -3,22 +3,20 @@ const { Select } = enquirer;
 
 export class Display {
   async selectAndDisplayDetail(memoList) {
-    const prompt = new Select({
-      message: "Choose a note you want to see:",
-      choices: memoList,
-    });
-
-    return prompt.run().then((answer) => {
-      const indexOfAnswer = prompt.choices.findIndex((memo) => {
-        return memo.title === answer;
-      });
-      console.log(prompt.choices[indexOfAnswer].detail);
-    });
+    const message = "Choose a note you want to see:";
+    const selectedMemo = await this.selectMemo(message, memoList);
+    console.log(selectedMemo.detail);
   }
 
   async selectMemoIdToDelete(memoList) {
+    const message = "Choose a memo you want to delete:";
+    const selectedMemo = await this.selectMemo(message, memoList);
+    return selectedMemo.id;
+  }
+
+  async selectMemo(message, memoList) {
     const prompt = new Select({
-      message: "Choose a memo you want to delete:",
+      message: message,
       choices: memoList,
     });
 
@@ -26,7 +24,7 @@ export class Display {
       const indexOfAnswer = prompt.choices.findIndex((memo) => {
         return memo.title === answer;
       });
-      return prompt.choices[indexOfAnswer].id;
+      return prompt.choices[indexOfAnswer];
     });
   }
 }
